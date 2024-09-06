1a97104ca304ae119f9d99bc1b796a508c478f37
ninadri <159666628+ninadri@users.noreply.github.com>
2024-09-06 16:31:57 -0500

    added code to the components and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Logo() {}
+function Logo() {
+  return <h1> 🏝️ Far Away 🧳 </h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
